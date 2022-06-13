65ec4638bd1735df8daeff2783bf6f18a3034288
Calvin Ho <calvin.ch.ho@gmail.com>
2022-06-13 19:33:34 -0400

    Added content to home and urls view pages and routes to those endpoints

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,8 +13,10 @@ app.get("/", (req, res) => {
   res.send("Hello!\n");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  //variables sent to an EJS template must be inside an object so that data within the template can be accessed by the key
+  const templateVars = { urls: urlDatabase }; 
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
