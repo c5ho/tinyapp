9a87e55f960c1375b84457640f27b5c6a5c42953
Calvin Ho <calvin.ch.ho@gmail.com>
2022-06-13 22:19:20 -0400

    Added new route for adding new longURLs which get parsed and added to URLdatabase

--- express_server.js
+++ express_server.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+    let randomString = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const stringLength = 6
+
+    for ( let i = 0; i < stringLength; i++ ) {
+      randomString += characters.charAt(Math.floor(Math.random()*characters.length));
+   }
+   return randomString
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,23 +34,33 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString()] = req.body.longURL;
 });
 
 // app.get("/hello", (req, res) => {
 //   res.end("<html><body>Hello <b>World</b></body></html>\n");
 // });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}\n`);
- });
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}\n`);
+//  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}\n`);
- });
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}\n`);
+//  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
